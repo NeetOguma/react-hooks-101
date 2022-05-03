c904539e381ef44a28a03acfe1215b3f5301b70c
NeetOguma <neetoguma@gmail.com>
2022-05-03 18:25:58 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
+
+  useEffect(() => {
+    console.log("これはレンダリングされると来る");
+  });
+
+  const renderPeriod = () => {
+    console.log("renderPeriod render period");
+    return "●";
+  };
+
+  useEffect(() => {
+    console.log("これは初回だけ");
+  }, []);
+
+  useEffect(() => {
+    console.log("これはnameが変わった時くる");
+  }, [name]);
+  // nameに関する変更が行われた時呼び出される
 
   return (
     <>
