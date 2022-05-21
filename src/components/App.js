beecb4f0dc6c5c64f67b83ab099d29d9a4128a56
NeetOguma <neetoguma@gmail.com>
2022-05-21 19:12:58 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import reducer from "../reducers";
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  // ２つの
+
+  // addEventを発火させるとページ全体がリロードされてしまう
+  const addEvent = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "CREATE_EVENT",
+      title,
+      body,
+    });
+    // dispatchはactionを引数でとる
+    // actionはindex.jsのCREATE_EVENT({ title: action.title, body: action.body })
+    // actionにはtypeの他にもtitleとbodyも必要
+    // titleとbodyは入力されたものを使用する。
+    setTitle("");
+    setBody(""); // stateのset〇〇に空文字を入れることで、投稿後リセットされるようになる
+  };
+  console.log({ state });
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -9,14 +32,26 @@ const App = () => {
         <div className="form-group">
           {/* bootStrapでは、forがhtmlForになる */}
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" />
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)} // e.target.valueは慣用句として覚える
+          />
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">ボディー</label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
         </div>
 
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={(e) => addEvent(e)}>
+          イベントを作成する
+        </button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
 
